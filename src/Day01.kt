ffdab47eb15c1b397e9a499e5ab284ad2bdc15f0
Oliver <oliver.simon.schmid@gmail.com>
2022-12-01 21:58:57 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun toCalorieList(input: List<String>): MutableList<Int> {
+        val list = mutableListOf<Int>()
+        var currAmount = 0
+
+        for ((index, str) in input.withIndex()) {
+            if (str.matches(Regex("[0-9]+"))) {
+                currAmount += Integer.parseInt(str)
+                if (index == input.size - 1) list.add(currAmount)
+            } else {
+                list.add(currAmount)
+                currAmount = 0
+            }
+        }
+        return list
     }
 
+    fun part1(input: List<String>): Int = toCalorieList(input).max()
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val topThree = toCalorieList(input).sortedDescending().slice(0..2)
+        return topThree.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val result1 = part1(testInput)
+    println(result1)
+    check(result1 == 68442)
+
+    val result2 = part2(testInput)
+    println(result2)
+    check(result2 == 204837)
+
+    /*
+    val input = readInput("Day")
     println(part1(input))
     println(part2(input))
+    */
 }
