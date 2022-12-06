84bd3ab0594225fc705efe42cdcca1fc63f986f8
Oliver <oliver.simon.schmid@gmail.com>
2022-12-06 17:18:22 +0100

    added alternative solution for Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -10,9 +10,16 @@ fun findMarker(signal: String, signalLength: Int = 4): Int {
     return -1
 }
 
+fun findMarkerAlternative(signal: String, signalLength: Int): Int {
+    signal.forEachIndexed { idx, _ ->
+        if (idx >= signalLength && signal.subSequence((idx - (signalLength - 1))..idx).toSet().size == signalLength) return idx + 1
+    }
+    return -1
+}
+
 fun main() {
     val input = readInput("Day06_text")
-    val result = findMarker(input.first())
+    val result = findMarker(input.first(), 4)
     println(result)
     check(result == 1093)
 
